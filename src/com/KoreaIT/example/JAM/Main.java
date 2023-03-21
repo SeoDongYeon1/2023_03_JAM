18b82bfd48f9cc33001cff50e0469b3bc95c6c58
SeoDongYeon1 <ehddus2655@gmail.com>
2023-03-21 15:08:16 +0900

    게시물 리스팅 시 데이터를 DB로부터 가져오기

--- src/com/KoreaIT/example/JAM/Main.java
+++ src/com/KoreaIT/example/JAM/Main.java
@@ -3,13 +3,13 @@ package com.KoreaIT.example.JAM;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-	static List<Article> articles = new ArrayList<>();
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -39,7 +39,6 @@ public class Main {
 				String body = sc.nextLine();
 				
 				Article article = new Article(id, title, body);
-				articles.add(article);
 				
 				// System.out.println(article); // ex) Article [id=1, title=제목, body=내용]
 				System.out.printf("%d번글이 생성되었습니다.\n", article.id);
@@ -93,13 +92,75 @@ public class Main {
 			else if(command.equals("article list")) {
 				System.out.println("==게시물 목록==");
 				
+				Connection conn = null;
+				PreparedStatement pstmt = null;
+				ResultSet rs = null;
+				
+				List<Article> articles = new ArrayList<>();
+				
+				try {
+					Class.forName("com.mysql.jdbc.Driver");
+					String url = "jdbc:mysql://127.0.0.1:3306/JAM?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+
+					conn = DriverManager.getConnection(url, "root", "");
+					System.out.println("연결 성공!");
+
+					String sql = "SELECT *";
+					sql += " FROM article";
+					sql += " ORDER BY id DESC;";
+
+					System.out.println(sql);
+
+					pstmt = conn.prepareStatement(sql);
+
+					rs = pstmt.executeQuery(sql);
+					
+					while(rs.next()) { // while(rs.next())는 다음 데이터가 없을때까지 반복
+						int id = rs.getInt("id");
+						String regDate = rs.getString("regDate");
+						String updateDate = rs.getString("updateDate");
+						String title = rs.getString("title");
+						String body = rs.getString("body");
+						
+						Article article = new Article(id, regDate, updateDate, title, body);
+						articles.add(article);
+						
+					}
+					
+				} catch (ClassNotFoundException e) {
+					System.out.println("드라이버 로딩 실패");
+				} catch (SQLException e) {
+					System.out.println("에러 : " + e);
+				} finally {
+					try { // close는 역순이 좋다
+						if (rs != null && !rs.isClosed()) {
+							rs.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+					try {
+						if (pstmt != null && !pstmt.isClosed()) {
+							pstmt.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+					try {
+						if (conn != null && !conn.isClosed()) {
+							conn.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+				
 				if(articles.size()==0) {
 					System.out.println("게시물이 없습니다.");
 					continue;
 				}		
 				System.out.println("번호 /   제목");
-				for(int i = articles.size()-1; i >= 0; i--) {
-					Article article = articles.get(i);
+				for(Article article : articles) {
 					System.out.printf("%d   /   %s  \n", article.id, article.title);
 				}
 			}
